43ed853cddd9eeab8e91484adafd46125b0a5aec
Yolo™ <timm.sauer@outlook.de>
2022-08-07 22:11:41 +0200

    initial

---
+++ android/app/src/main/kotlin/com/yolo/tagmusicplayer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yolo.tagmusicplayer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:audio_visualizer/audio_visualizer.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+        ),
+        body: Container(
+          child: const Text("awas"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.access_alarm_outlined),
+          onPressed: () {
+            print("asd");
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.abc),
+              label: "aosdd",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.accessibility_new_rounded),
+              label: "aosaaaaaaaaaaaadd",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_box_outlined),
+              label: "aosdd",
+            ),
+          ],
+        ),
+        drawer: Drawer(
+          child: Text("asaaaaa"),
+        ),
+      ),
+    );
+  }
+}
